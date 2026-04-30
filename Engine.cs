8f29a4411b3f905eb8cc49029564b70448fce5a6
maxly413 <maxly746@gmail.com>
2026-04-30 10:49:55 +1000

    Add files via upload

--- Engine.cs
+++ Engine.cs
@@ -7,8 +7,12 @@ namespace Engine
     {
         public static bool IsMouseButtonLeftClicked()
         {
-            if (Raylib.IsMouseButtonPressed(MouseButton.Left)) return true;
-            return false;
+            return Raylib.IsMouseButtonPressed(MouseButton.Left);
+        }
+
+        public static bool IsMouseButtonLeftUp()
+        {
+            return Raylib.IsMouseButtonUp(MouseButton.Left);
         }
     }
 
@@ -23,30 +27,32 @@ namespace Engine
             sw.Start();
         }
 
+        // Function for calculating dt which is the amount of time in seconds between frames
         public static float GetDeltaTime()
         {
-            currentTime = sw.Elapsed.TotalSeconds;
-            float deltaTime = (float)(currentTime - lastTime);
-            lastTime = currentTime;
+            currentTime = sw.Elapsed.TotalSeconds; // Stores current time
+            float deltaTime = (float)(currentTime - lastTime); // Calculates time since last frame by subtracing the current time with the time the last frame was at
+            lastTime = currentTime; // Stores current time again for the next frame to use 
             return deltaTime;
         }
     }
 
     public class Cooldown
     {
-        public float Duration { get; set; }
-        public float _timer = 0;
+        public float Duration { get; set; } // cooldown duration
+        public float _timer = 0; // Cooldown's internal timer which accumulates how much time has passed
         public Cooldown(float seconds)
         {
             Duration = seconds;
         }   
 
+        // Checks if cooldown is finished
         public bool IsReady(float dt)
         {
-            _timer += dt;
-            if (_timer >= Duration)
+            _timer += dt; // Adds time passed since last fame to _timer
+            if (_timer >= Duration) // If the time accumulating in total is >= cooldown duration the cooldown has ended
             {
-                _timer = 0;
+                _timer = 0; // Reset the _timer to start new cooldown
                 return true;
             }
             return false;
